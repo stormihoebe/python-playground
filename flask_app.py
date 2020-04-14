2692849d6692a260d96aaf1c93fcb94cc1f699b1
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2020-04-13 17:00:44 -0700

    add flask app route for update db

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from get_person_lambda import lambda_handler
+from database_manager import update_databases_with_csv
+import simplejson as json
+import pprint
+
 app = Flask(__name__)
+
+
+def pretty_return(obj):
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(json.dumps(obj))
+    return obj
 
 # setup routes
 
@@ -8,13 +18,20 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     resp = lambda_handler()
-    return resp
+    return pretty_return(resp)
 
 
 @app.route('/<person_id>')
 def person(person_id):
 
-    return lambda_handler(int(person_id))
+    return pretty_return(lambda_handler(int(person_id)))
+
+
+@app.route('/update-databases')
+def update():
+
+    obj = update_databases_with_csv()
+    return pretty_return(obj)
 
 if __name__ == "__main__":
     app.run(debug=True)
